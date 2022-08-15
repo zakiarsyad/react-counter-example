af805f4ba11a4e891742de63fe1cb1f1ce0e0986
zakiarsyad <zakiarsyad@gmail.com>
2022-08-15 19:00:21 +0700

    create readt counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const minus = () => {
+    console.log('minus click')
+    setCounter(counter - 1)
+  }
+
+  const add = () => {
+    console.log('add click')
+    setCounter(counter + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={minus}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={add}>+</button>
     </div>
   );
 }
